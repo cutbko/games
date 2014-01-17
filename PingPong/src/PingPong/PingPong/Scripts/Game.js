32685378dc8e418f9c0a3c1c5d97e3de7b5332e6
cutbko <kutsenko.eugene@hotmail.com>
2014-01-17 17:35:45 +0200

    Add player rects

--- PingPong/src/PingPong/PingPong/Scripts/Game.js
+++ PingPong/src/PingPong/PingPong/Scripts/Game.js
@@ -1,10 +1,11 @@
-﻿var gameCreator = (function (jQuery, document) {
+﻿var pingPong = (function (jQuery, document) {
 
-    function gameEngine(update, draw, canvasId) {
+    var canvas;
+
+    function gameEngine(update, draw) {
         this.frameRate = 60;
         
-        var context = document.getElementById(canvasId)
-                              .getContext("2d");
+        var context = canvas.getContext("2d");
 
         var isPaused = false;
         var lastUpdateTime = null;
@@ -14,10 +15,11 @@
             intervalId = setInterval(function() {
                                          var currentUpdateTime = new Date();
                                          if (!isPaused && lastUpdateTime != null) {
-                                             context.clearRect(0, 0, 500, 500);
+                                             context.clearRect(0, 0, canvas.width, canvas.height);
                                              update(currentUpdateTime.getTime() - lastUpdateTime.getTime());
                                              draw(context);
                                          }
+
                                          lastUpdateTime = currentUpdateTime;
                                      },
                                      1000 / this.frameRate);
@@ -39,21 +41,55 @@
         };
     }
 
+    function player(left, top, width, height) {
+        this.left = left;
+        this.top = top;
+        this.width = width;
+        this.height = height;
+
+        this.setPosition = function(x, y) {
+            this.left = x - width / 2;
+            this.top = y - height / 2;
+        };
+
+        this.update = function (elapsed) {
+        };
+
+        this.draw = function (context) {
+            context.beginPath();
+            context.lineWidth = "5";
+            context.strokeStyle = "black";
+            context.rect(this.left, this.top, this.width, this.height);
+            context.stroke();
+        };
+    }
+
+    var player1 = new player(10, 10, 20, 50);
+    var player2 = new player(100, 10, 20, 50);
+    var ge;
+
     return {
-        create: function (canvasId) {
-            var timeForFrame;
-            var ge = new gameEngine(function(elapsed) {
-                                        timeForFrame = elapsed;
-                                    },
-                                    function(context) {
-                                        context.fillText(timeForFrame, 10, 10);
-                                    },
-                                    canvasId);
+        init: function (canvasId) {
+            canvas = document.getElementById(canvasId);
+            
+            ge = new gameEngine(function (elapsed) {
+                                    player1.update(elapsed);
+                                    player2.update(elapsed);
+                                },
+                                function (context) {
+                                    player1.draw(context);
+                                    player2.draw(context);
+                                },
+                                canvasId);
             ge.start();
+        },
 
-            return {
+        setPlayer1Position: function (x, y) {
+            player1.setPosition(x, y);
+        },
 
-            };
+        setPlayer2Position: function (x, y) {
+            player2.setPosition(x, y);
         }
     };
 
