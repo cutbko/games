5b044767744820a9fdc57dc51543ef561eb2e596
cutbko <kutsenko.eugene@hotmail.com>
2014-01-23 19:05:39 +0200

    make game adaptive to screen size

--- PingPong/src/PingPong/PingPong/Scripts/Modules/pingPongFactory.js
+++ PingPong/src/PingPong/PingPong/Scripts/Modules/pingPongFactory.js
@@ -1,7 +1,8 @@
 ﻿define(["geometry", "gameEngineFactory"], function (geometry, gameEngineFactory) {
+    var GAME_WIDTH = 16;
+    var GAME_HEIGHT = 9;
 
-    console.log(geometry);
-    console.log(gameEngineFactory);
+    var scale = 1;
 
     function Player(rectangle) {
         this.rectangle = rectangle;
@@ -13,7 +14,7 @@
             context.beginPath();
             context.lineWidth = "5";
             context.strokeStyle = "black";
-            context.rect(this.rectangle.left, this.rectangle.top, this.rectangle.width, this.rectangle.height);
+            context.rect(this.rectangle.left * scale, this.rectangle.top * scale, this.rectangle.width * scale, this.rectangle.height * scale);
             context.stroke();
         };
     }
@@ -21,8 +22,8 @@
     function Ball(circle) {
 
         this.circle = circle;
-        this.dx = 500;
-        this.dy = 500;
+        this.dx = 5;
+        this.dy = 5;
 
         this.update = function(elapsed) {
             this.circle.offset(this.dx * elapsed,
@@ -33,9 +34,9 @@
             context.beginPath();
             context.lineWidth = "5";
             context.strokeStyle = "black";
-            context.arc(this.circle.x,
-                        this.circle.y,
-                        this.circle.radius,
+            context.arc(this.circle.x * scale,
+                        this.circle.y * scale,
+                        this.circle.radius * scale,
                         0,
                         Math.PI * 2);
             context.stroke();
@@ -46,11 +47,14 @@
 
         var canvas = document.getElementById(canvasId);
 
-        var player1 = new Player(new geometry.Rectangle(0, 0, 10, 100));
-        player1.rectangle.setCenterPosition(canvas.width / 4, canvas.height / 2);
-        var player2 = new Player(new geometry.Rectangle(0, 0, 10, 100));
-        player1.rectangle.setCenterPosition(3 * canvas.width / 4, canvas.height / 2);
-        var ball = new Ball(new geometry.Circle(100, 100, 50));
+        var player1 = new Player(new geometry.Rectangle(0, 0, GAME_WIDTH / 64, GAME_HEIGHT / 9));
+        player1.rectangle.setCenterPosition(GAME_WIDTH / 4, GAME_HEIGHT / 2);
+        var player2 = new Player(new geometry.Rectangle(0, 0, GAME_WIDTH / 64, GAME_HEIGHT / 9));
+        player2.rectangle.setCenterPosition(3 * GAME_WIDTH / 4, GAME_HEIGHT / 2);
+        var ball = new Ball(new geometry.Circle(GAME_WIDTH / 2, GAME_HEIGHT / 2, Math.sqrt(GAME_WIDTH * GAME_WIDTH + GAME_HEIGHT * GAME_HEIGHT) / 64));
+
+        console.log(player1.rectangle);
+        console.log(player2.rectangle);
 
         this.setPlayer1Position = function(x, y) {
             player1.rectangle.setCenterPosition(x, y);
@@ -60,18 +64,20 @@
             player2.rectangle.setCenterPosition(x, y);
         };
 
-        function getCenter() {
-            return canvas.width / 2;
-        }
-
         function update(elapsed) {
+            if (canvas.width / canvas.height - GAME_WIDTH / GAME_HEIGHT > 0.01) {
+                throw "canvas must be in proportion 16 : 9";
+            }
+
+            scale = canvas.width / GAME_WIDTH;
+
             player1.update(elapsed);
             player2.update(elapsed);
             ball.update(elapsed);
 
             ensureBallIsOnScreen();
-            ensurePlayerIsOnItsSide(player1, new geometry.Rectangle(0, 0, getCenter(), canvas.height));
-            ensurePlayerIsOnItsSide(player2, new geometry.Rectangle(getCenter(), 0, canvas.width / 2, canvas.height));
+            ensurePlayerIsOnItsSide(player1, 0, GAME_WIDTH / 2);
+            ensurePlayerIsOnItsSide(player2, GAME_WIDTH / 2, GAME_WIDTH);
 
             checkIfPlayerIntersectsBall(player1);
             checkIfPlayerIntersectsBall(player2);
@@ -103,8 +109,8 @@
 
             context.rect(0, 0, canvas.width, canvas.height);
 
-            context.moveTo(getCenter() - 2, 0);
-            context.lineTo(getCenter() - 2, canvas.height);
+            context.moveTo(canvas.width / 2 - 2, 0);
+            context.lineTo(canvas.width / 2 - 2, canvas.height);
             context.stroke();
 
             player1.draw(context);
@@ -117,7 +123,7 @@
                 ball.dx = Math.abs(ball.dx);
             }
 
-            if (ball.circle.getRight() > canvas.width) {
+            if (ball.circle.getRight() > GAME_WIDTH) {
                 ball.dx = -Math.abs(ball.dx);
             }
 
@@ -125,26 +131,26 @@
                 ball.dy = Math.abs(ball.dy);
             }
 
-            if (ball.circle.getBottom() > canvas.height) {
+            if (ball.circle.getBottom() > GAME_HEIGHT) {
                 ball.dy = -Math.abs(ball.dy);
             }
         }
 
-        function ensurePlayerIsOnItsSide(player, rectangle) {
-            if (player.rectangle.left < rectangle.left) {
-                player.rectangle.left = rectangle.left;
+        function ensurePlayerIsOnItsSide(player, left, right) {
+            if (player.rectangle.left < left) {
+                player.rectangle.left = left;
             }
 
-            if (player.rectangle.top < rectangle.top) {
-                player.rectangle.top = rectangle.top;
+            if (player.rectangle.top < 0) {
+                player.rectangle.top = 0;
             }
 
-            if (player.rectangle.getBottom() > rectangle.getBottom()) {
-                player.rectangle.setBottom(rectangle.getBottom());
+            if (player.rectangle.getBottom() > GAME_HEIGHT) {
+                player.rectangle.setBottom(GAME_HEIGHT);
             }
 
-            if (player.rectangle.getRight() > rectangle.getRight()) {
-                player.rectangle.setRight(rectangle.getRight());
+            if (player.rectangle.getRight() > right) {
+                player.rectangle.setRight(right);
             }
         }
 
