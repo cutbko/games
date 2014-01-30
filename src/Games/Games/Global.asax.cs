625252c67aea8e6f3c40781f9051871581843afd
cutbko <kutsenko.eugene@hotmail.com>
2014-01-30 15:42:03 +0200

    add web api and empty ping pong api controller

--- src/Games/Games/Global.asax.cs
+++ src/Games/Games/Global.asax.cs
@@ -1,4 +1,5 @@
 ﻿using System.Web;
+using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
@@ -13,6 +14,7 @@ namespace Games
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            GlobalConfiguration.Configure(WebApiConfig.Register);
         }
     }
 }
